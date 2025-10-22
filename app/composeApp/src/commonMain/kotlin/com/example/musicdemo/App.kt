39f1bd4d65d6310a17b092525567a83bd3b45181
Daria Kolodzey <kolodaria@gmail.com>
2025-10-22 03:00:15 +0200

    dev: Full functionality on Android

--- app/composeApp/src/commonMain/kotlin/com/example/musicdemo/App.kt
+++ app/composeApp/src/commonMain/kotlin/com/example/musicdemo/App.kt
@@ -1,6 +1,7 @@
 package com.example.musicdemo
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -10,10 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import eu.iamkonstantin.kotlin.gadulka.GadulkaPlayerState
+import eu.iamkonstantin.kotlin.gadulka.isPlaying
+import eu.iamkonstantin.kotlin.gadulka.rememberGadulkaLiveState
 import eu.iamkonstantin.kotlin.gadulka.rememberGadulkaState
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -21,33 +26,70 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import musicdemo.composeapp.generated.resources.Res
 import musicdemo.composeapp.generated.resources.compose_multiplatform
 
+enum class Track(val title: String, val url: String) {
+    ENGLISH("English", "https://github.com/Oktosha/kotlin-music/raw/refs/heads/main/music-files/english-count-to-10.mp3"),
+    SPANISH("Spanish", "https://github.com/Oktosha/kotlin-music/raw/refs/heads/main/music-files/spanish-count-to-10.mp3"),
+    DUTCH("Dutch", "https://github.com/Oktosha/kotlin-music/raw/refs/heads/main/music-files/dutch-count-to-10.mp3"),
+}
+
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            val player = rememberGadulkaState()
-            Row {
+    Column {
+        val gadulka = rememberGadulkaLiveState()
+        var activeTrack by remember {  mutableStateOf(Track.ENGLISH) }
+        var sliderValueRaw by remember { mutableStateOf(0F) }
+        Text("This app can play 3 audio tracks: counting to 10 in English, Spanish and Dutch.")
+        Text(gadulka.state.name)
+
+        Text("Volume: ${gadulka.volume}")
+
+        Text("Position: ${gadulka.position / 1000}s / ${gadulka.duration / 1000}s")
+        Text("Slider value raw ${sliderValueRaw}")
+        Row {
+            for (track in Track.entries) {
                 Button(
                     onClick = {
-                        player.play(
-                            "https://download.samplelib.com/wav/sample-12s.wav"
-                        )
+                        activeTrack = track
+                        gadulka.player.play(track.url)
                     }) {
-                    Text("Play")
+                    Text(if (activeTrack == track) track.title.uppercase() else track.title)
                 }
-                Button(
-                    onClick = {
-                        player.stop()
-                    }) {
-                    Text("Stop")
-                }
+            }
+        }
+        Row {
+            Text("Playback position: ")
+            Slider(gadulka.position.toFloat() / if (gadulka.duration.toFloat() == 0F) 1F else gadulka.duration.toFloat(),
+                onValueChange = {
+                    sliderValueRaw = it
+                },
+                onValueChangeFinished = {
+                val position = (sliderValueRaw * gadulka.duration).toLong()
+                gadulka.player.seekTo(position)
+            })
+        }
+        Row {
+            Button(
+                onClick = {
+                    if (gadulka.position > 0)
+                        gadulka.player.play()
+                    else
+                        gadulka.player.play(activeTrack.url)
+                }) {
+                Text("Play")
+            }
+            Button(
+                onClick = {
+                    gadulka.player.pause()
+                }) {
+                Text("Pause")
+            }
+            Button(
+                onClick = {
+                    gadulka.player.play(activeTrack.url);
+                    gadulka.player.pause();
+                }) {
+                Text("Stop")
             }
         }
     }
